b65dc5c9701b642718cc0a8f890649d99890ab4b
Misael Miranda <66282432+misereitor@users.noreply.github.com>
2021-01-21 17:57:22 -0300

    cora

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,11 +17,6 @@ class Header extends Component {
                             <Link to='/'>Oficina</Link>
                             <Link to='/'>Como chegar</Link>
                         </div>
-                        <div className="social">
-                            <a href="https://api.whatsapp.com/send?phone=557588591466" target="blank"><i className="fa fa-whatsapp"></i>(75) 9 8859 1466</a>
-                            <a href="https://www.facebook.com/gazomotocenter/"  target="blank"><i className="fa fa-facebook"></i>fb.com/gazomotocenter</a>
-                            <a href="https://www.facebook.com/gazomotocenter/"  target="blank" className="instagram"><img src="./img/ico/Instagram-Icon.png" alt="Icone Instagram"/>@gazomotocenter</a>
-                        </div>
                     </div>
                 </header>
             </div>
